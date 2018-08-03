f97fad7ad26ab2f02f264dce98150fa215f33cfc
Morssel <kleinjak6@gmail.com>
2018-08-03 15:07:06 -0500

    added sending messages

---
+++ rockBlockMessageToSpace.py
@@ -0,0 +1,29 @@
+import rockBlock
+ 
+from rockBlock import rockBlockProtocol
+ 
+class mtExample (rockBlockProtocol):
+    
+    def main(self):
+        
+        rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
+             
+        rb.messageCheck()
+                                                                  
+        rb.close()
+           
+    def rockBlockRxStarted(self):
+        print("rockBlockRxStarted")
+        
+    def rockBlockRxFailed(self):
+        print("rockBlockRxFailed")
+
+    def rockBlockRxReceived(self,mtmsn,data):
+        print(f"rockBlockRxReceived  {mtmsn} {data}")
+        
+    def rockBlockRxMessageQueue(self,count):
+        print(f"rockBlockRxMessageQueue {count}")
+             
+        
+if __name__ == '__main__':
+    mtExample().main()
