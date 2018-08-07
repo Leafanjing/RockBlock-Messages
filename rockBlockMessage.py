a1f554c5b20ea82850567283b3abce25364b8fcf
Morssel <kleinjak6@gmail.com>
2018-08-07 13:46:59 -0500

    made 3.6 changes to rockBlockMessage.py

--- rockBlockMessage.py
+++ rockBlockMessage.py
@@ -6,22 +6,22 @@ class MoExample (rockBlockProtocol):
     
     def main(self):
 
-        userString = raw_input('Enter the message RockBlock will send to the Irdidium Satellite Network\n')
+        userString = input('Enter the message RockBlock will send to the Irdidium Satellite Network\n')
 
         rb = rockBlock.rockBlock("/dev/ttyUSB0", self)
         
-        rb.sendMessage(userString)      
-        
-	    rb.close()
+        rb.sendMessage(userString)
+
+        rb.close()
         
     def rockBlockTxStarted(self):
-        print "rockBlockTxStarted"
+        print("rockBlockTxStarted")
         
     def rockBlockTxFailed(self):
-        print "rockBlockTxFailed"
+        print("rockBlockTxFailed")
         
-    def rockBlockTxSuccess(self,momsn):
-        print "rockBlockTxSuccess " + str(momsn)
-        
+    def rockBlockTxSuccess(self, momsn):
+        print(f"rockBlockTxSuccess {momsn}")
+
 if __name__ == '__main__':
     MoExample().main()
